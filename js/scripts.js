5a6bd288b8a7b98e4da4024a3e76f08210fbdea5
Rakhee Gandhi <rpgandhi@gmail.com>
2017-09-29 11:21:35 -0700

    Build initial HTML form template and start to add js functionality.

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+function Pizza (size, toppings, cost, orderNumber) {
+  this.pizzaSize = size;
+  this.pizzaToppings = toppings;
+  this.pizzaCost = cost;
+  this.pizzaOrderNumber = orderNumber;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+
+  $("#pizzaForm").submit(function(event){
+    event.preventDefault();
+
+
+
+
+
+
+
+  }):
+});
