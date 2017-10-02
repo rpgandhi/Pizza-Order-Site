9240aa23cf779283837560824b20c478bab8527d
Rakhee Gandhi <rpgandhi@gmail.com>
2017-10-01 21:27:31 -0700

    Try to resolve issues with no luck.

--- js/scripts.js
+++ js/scripts.js
@@ -1,134 +1,79 @@
-// function Pizza (name, size, toppings, orderNumber) {
-//   this.personName = name;
-//   this.pizzaSize = size;
-//   this.pizzaToppings = toppings;
-//   this.pizzaOrderNumber = orderNumber;
-//   this.pizzaRunningTotal = [];
-//   this.pizzaGrandTotal = 0;
-// }
-//
-// Pizza.prototype.getOrderNumber=function(min, max) {
-//   min = Math.ceil(111111);
-//   max = Math.floor(999999);
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-//
-// Pizza.prototype.grandTotal = function() {
-//   var grandTotal = this.pizzaGrandTotal;
-//   if (this.pizzaRunningTotal.length > 0) {
-//   var sum = this.pizzaRunningTotal.reduce((a, b)=> a+b);
-//   this.pizzaGrandTotal = (sum + grandTotal);
-//   }
-// }
-//
-// // Pizza.prototype.showName = function() {
-// //   var nameOutput = newUserName.personName;
-// //   return nameOutput;
-// // }
-//
-//
-// $(document).ready(function(){
-//
-//   // var userName = $("input#newName").val();
-//   // var orderNum = userName.getOrderNumber();
-//   // var userName = $("input#newName").val();
-//   var orderNum = Pizza.prototype.getOrderNumber();
-//   var newOrderNum = new Pizza(orderNum);
-//
-//
-//   $("#pizzaForm").submit(function(event){
-//     event.preventDefault();
-//
-//
-//    $('input:checkbox[name=toppings]:checked').each(function(){
-//       var userToppings = $(this).val();
-//     });
-//     var userSize = $("input:radio[name=size]:checked").val();
-//     var userName = $("input#newName").val();
-//
-//
-//
-//     var newUserName = new Pizza(userName, userSize, userToppings, orderNum);
-//
-//     alert(orderNum);
-//
-//     $("#orderConfirmation").show(orderNum);
-//     // alert(newUserName.personName);
-//     // alert("working");
-//     $("#orderNameConfirmation").text(newUserName.personName);
-//
-//
-//
-//   });
-// });
-
-
-
-
-function Pizza (name, size, topping) {
-  this.name = name;
-  this.size = size;
-  this.topping = topping;
-  this.cost = [];
-  this.grandTotal = [];
-}
-
+function Pizza (name, size, toppings, orderNumber, runningTotal, grandTotal) {
+  this.personName = name;
+  this.pizzaSize = size;
+  this.pizzaToppings = [];
+  this.pizzaOrderNumber = orderNumber;
+  this.pizzaRunningTotal = [];
+  this.pizzaGrandTotal = grandTotal;
+};
 
 Pizza.prototype.getOrderNumber=function(min, max) {
   min = Math.ceil(111111);
   max = Math.floor(999999);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
+//number of items in the toppings array to calculate Cost
+Pizza.prototype.numberToppings = function() {
+  var toppingsTotal = 0;
+  this.pizzaToppings.forEach(function() {
+  toppingsTotal += 2;
+  });
+  return toppingsTotal;
+  alert("toppingsTotalWorking");
+};
 
+// Cost calculator prototype
+Pizza.prototype.cost = function() {
+  var runningTotal = 0;
 
-$(document).ready(function() {
+  if (this.pizzaSize === "small") {
+    runningTotal = 8;
 
-  $(".chooseSize").change(function() {
-  if ($(this).is(':checked')) {
+  } else if (this.pizzaSize === "medium") {
+    runningTotal = 10;
 
-	var sizeCost = ($(this).val());
-
-  newOrder.cost.push(sizeCost);
-  alert(sizeCost);
+  } else if (this.pizzaSize === "large") {
+    runningTotal = 12;
 
   }
-});
+  runningTotal += (this.numberToppings());
+  return runningTotal;
+  alert("runningWorking");
+};
+
+$(document).ready(function(){
+
+  // var userName = $("input#newName").val();
+  // var orderNum = userName.getOrderNumber();
+  // var userName = $("input#newName").val();
+  var orderNum = Pizza.prototype.getOrderNumber();
+  // var newOrderNum = new Pizza(orderNum);
 
 
-  // var inputtedName = $("input#newName").val();
-  // var inputtedSize = $("input#newSize").val();
-  // var inputtedTopping = $("input#newTopping").val();
-  // var inputtedSizeCost = parseInt($("input:radio[name=small]:checked").val());
-  // var inputtedSizeInt = parseInt(inputtedSizeCost);
-  // var newOrder = new Pizza(inputtedName, inputtedSize, inputtedTopping);
-
-
-  $("form#pizzaForm").submit(function(event) {
+  $("#pizzaForm").submit(function(event){
     event.preventDefault();
 
-    var inputtedName = $("input#newName").val();
-    var inputtedSize = $("input#newSize").val();
-    var inputtedTopping = $("input#newTopping").val();
-    var inputtedSizeCost = $("input[type=radio][name=small][id=small]:checked").val();
-    var orderNum = Pizza.prototype.getOrderNumber();
+
+   $("input:checkbox[name=toppings]:checked").each(function(){
+      var userToppings = $(this).val();
+      userToppings.push(parseInt($(this).val()));
+    });
+    var userSize = $("input:radio[name=size]:checked").val();
+    var userName = $("input#newName").val();
+
+
+
+    var newUserName = new Pizza(userName, userSize, userToppings, orderNum);
+
     alert(orderNum);
 
-
-    var newOrder = new Pizza(inputtedName, inputtedSize, inputtedTopping);
-    alert(inputtedSizeCost);
-    newOrder.cost.push(inputtedSizeCost);
-
-    alert(inputtedSizeCost);
+    $("#orderConfirmation").show(orderNum);
+    // alert(newUserName.personName);
+    // alert("working");
+    $("#orderNameConfirmation").text(newUserName.personName);
 
 
 
-    $("ul#orderConfirmation").append("<li><span class='contact'>" + newOrder.name + "</span></li>");
-    $("ul#orderConfirmation").append("<li><span class='contact'>" + newOrder.size + "</span></li>");
-    $("ul#orderConfirmation").append("<li><span class='contact'>" + newOrder.topping + "</span></li>");
-
-    $("input#newName").val("");
-    $("input#newSize").val("");
-    $("input#newTopping").val("");
   });
 });
